622f6718319fdb1a8fba925c358d9ab7ef1dedf8
Jaroslav Málek <jaroslav.malek@quanti.cz>
2022-06-23 15:06:20 +0200

    Initial commit

--- src/main/java/com/example/kstreamserrordemo/KStreamsErrorDemoApplication.java
+++ src/main/java/com/example/kstreamserrordemo/KStreamsErrorDemoApplication.java
@@ -2,12 +2,13 @@ package com.example.kstreamserrordemo;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.ValueTransformer;
 import org.apache.kafka.streams.kstream.ValueTransformerWithKey;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.state.StoreBuilder;
 import org.apache.kafka.streams.state.Stores;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.function.Consumer;
 
@@ -16,47 +17,62 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class KstreamsErrorDemoApplication {
+public class KStreamsErrorDemoApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(KStreamsErrorDemoApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(KstreamsErrorDemoApplication.class, args);
+        SpringApplication.run(KStreamsErrorDemoApplication.class, args);
     }
 
+    public static final String STORE_1_NAME = "store1";
+    public static final String STORE_2_NAME = "store2";
 
     @Bean
     public StoreBuilder<KeyValueStore<String, String>> store1() {
         return Stores.keyValueStoreBuilder(
-                Stores.persistentKeyValueStore("store1"), Serdes.String(), Serdes.String());
+                Stores.persistentKeyValueStore(STORE_1_NAME), Serdes.String(), Serdes.String());
     }
 
     @Bean
     public Consumer<KStream<String, String>> app1() {
-        return s -> s.transform().foreach();
+        return s -> s
+                .transformValues(() -> new UppercaseTransformer(STORE_1_NAME), STORE_1_NAME)
+                .foreach((k, v) -> log.info("Stored {} -> {} into {}", k, v, STORE_1_NAME));
     }
 
     @Bean
     public StoreBuilder<KeyValueStore<String, String>> store2() {
         return Stores.keyValueStoreBuilder(
-                Stores.persistentKeyValueStore("store2"), Serdes.String(), Serdes.String());
+                Stores.persistentKeyValueStore(STORE_2_NAME), Serdes.String(), Serdes.String());
+    }
+
+    @Bean
+    public Consumer<KStream<String, String>> app2() {
+        return s -> s
+                .transformValues(() -> new UppercaseTransformer(STORE_2_NAME), STORE_2_NAME)
+                .foreach((k, v) -> log.info("Stored {} -> {} into {}", k, v, STORE_2_NAME));
     }
 
     static class UppercaseTransformer implements ValueTransformerWithKey<String, String, String> {
 
-        private static final String storeName;
+        private final String storeName;
         private KeyValueStore<String, String> store;
 
-        public UppercaseTransformer(KeyValueStore<String, String> store) {
-            this.store = store;
+        UppercaseTransformer(String storeName) {
+            this.storeName = storeName;
         }
 
         @Override
         public void init(ProcessorContext context) {
-            this.store = context.getStateStore()
+            this.store = context.getStateStore(storeName);
         }
 
         @Override
-        public String transform(String readOnlyKey, String value) {
-            return null;
+        public String transform(String key, String value) {
+            var result = value.toUpperCase();
+            store.put(key, result);
+            return result;
         }
 
         @Override
--- src/test/java/com/example/kstreamserrordemo/KStreamsErrorDemoApplicationTests.java
+++ src/test/java/com/example/kstreamserrordemo/KStreamsErrorDemoApplicationTests.java
@@ -1,14 +1,69 @@
 package com.example.kstreamserrordemo;
 
-import org.junit.jupiter.api.Test;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.awaitility.core.ConditionTimeoutException;
+import org.junit.jupiter.api.RepeatedTest;
 
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
+import org.springframework.cloud.stream.binder.kafka.streams.KafkaStreamsRegistry;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+import static com.example.kstreamserrordemo.KStreamsErrorDemoApplication.STORE_2_NAME;
+import static org.awaitility.Awaitility.await;
+import static org.awaitility.Awaitility.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
-class KstreamsErrorDemoApplicationTests {
+@EmbeddedKafka(topics = { "input-1", "input-2"})
+class KStreamsErrorDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
+    private static final String KEY = "key";
+    @Autowired
+    private KafkaTemplate<String, String> template;
+
+    @Autowired
+    private InteractiveQueryService queryService;
+
+    @Autowired
+    private KafkaStreamsRegistry registry;
+
+    @RepeatedTest(5) // sometimes it might work on KStreams 3.1 if the stars are well aligned
+    void sendMessageAndAssertStore() {
+
+        waitForRunningStreams();
+
+        var testMessage = "Ahoy!";
+        template.send("input-2", KEY, testMessage);
+
+        // wait until the store has something or timeout passes
+        try {
+            given()
+                    .ignoreExceptions()
+                    .await()
+                    .atMost(Duration.ofSeconds(10))
+                    .until(() -> {
+                        var store = queryService.getQueryableStore(STORE_2_NAME, QueryableStoreTypes.keyValueStore());
+                        return store.get(KEY) != null;
+                    });
+        } catch (ConditionTimeoutException e) {
+            // ignored
+        }
+
+        // this will (usually) throw "The state store, store2, may have migrated to another instance." on KStreams 3.1
+        var store = queryService.getQueryableStore(STORE_2_NAME, QueryableStoreTypes.keyValueStore());
+        assertThat(store.get(KEY), equalTo(testMessage.toUpperCase()));
     }
 
+    private void waitForRunningStreams() {
+        await()
+                .atMost(Duration.ofSeconds(60))
+                .until(() -> registry.streamsBuilderFactoryBeans().stream().allMatch(x -> x.getKafkaStreams().state().equals(KafkaStreams.State.RUNNING)));
+    }
 }
